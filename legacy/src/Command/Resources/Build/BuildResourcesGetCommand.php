896eb7289ac25d58f5c6b689cfce9233f2a93ee6
Matthias Van Woensel <3532563+matthiaz@users.noreply.github.com>
2026-04-21 14:35:31 +0200

    fix: Clarify why certain functions are not available for upsun fixed

--- legacy/src/Command/Resources/Build/BuildResourcesGetCommand.php
+++ legacy/src/Command/Resources/Build/BuildResourcesGetCommand.php
@@ -42,7 +42,12 @@ class BuildResourcesGetCommand extends CommandBase
     {
         $selection = $this->selector->getSelection($input);
         if (!$this->api->supportsSizingApi($selection->getProject())) {
-            $this->stdErr->writeln(sprintf('The flexible resources API is not enabled for the project %s.', $this->api->getProjectLabel($selection->getProject(), 'comment')));
+            $this->stdErr->writeln(sprintf(
+                'The flexible resources API is not enabled for the project %s.' . "\n"
+                . 'The function you attempted to use is not available on upsun fixed (platformsh).' . "\n"
+                . 'Please refer to the Upsun fixed (platformsh) documentation: https://fixed.docs.upsun.com/',
+                $this->api->getProjectLabel($selection->getProject(), 'comment')
+            ));
             return 1;
         }
 
