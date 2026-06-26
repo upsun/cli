4319d05a36d8e234bdb70110ac3f9a7ca6d58ff1
Michał Haracewiat <michal.haracewiat@platform.sh>
2026-06-26 10:30:47 +0200

    feat(integration): add redaction field for httplog and otlplog

--- legacy/src/Command/Integration/IntegrationCommandBase.php
+++ legacy/src/Command/Integration/IntegrationCommandBase.php
@@ -620,6 +620,21 @@ abstract class IntegrationCommandBase extends CommandBase
                 'required' => false,
                 'avoidQuestion' => true,
             ]),
+            'redaction' => new BooleanField('Redaction', [
+                'conditions' => ['type' => [
+                    'httplog',
+                    'newrelic',
+                    'splunk',
+                    'sumologic',
+                    'syslog',
+                    'otlplog',
+                ]],
+                'description' => 'Whether to enable built-in PII redaction (e.g. email addresses) before forwarding log lines',
+                'questionLine' => 'Should built-in PII redaction be enabled',
+                'default' => false,
+                'required' => false,
+                'avoidQuestion' => true,
+            ]),
             'headers' => new ArrayField('HTTP header', [
                 'optionName' => 'header',
                 'conditions' => ['type' => [
